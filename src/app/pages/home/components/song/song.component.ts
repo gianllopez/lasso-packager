cb35e53194d2ef972ec573942544b5b70ca9a77e
Gian López <lopezarizagianlucas@gmail.com>
2021-06-20 20:29:49 -0500

    master: i restructured some parts of the nabvar and i am passing with the song component.

--- src/app/pages/home/components/song/song.component.ts
+++ src/app/pages/home/components/song/song.component.ts
@@ -9,10 +9,16 @@ export class SongComponent {
 
   @Input() song!: Song;
   isfav!: boolean;
+  downloading!: boolean;
 
   formatTitle = () => this.song?.title.replace(' ', '-').toLowerCase();
 
   getIcon = (icon: string) => `'../../../../../assets/icons/${icon}.svg`;
+
+  setDownload() {
+    this.downloading = !this.downloading;
+    console.log('Fetching download...');
+  };
 
   setFav() {
     this.isfav = !this.isfav;
