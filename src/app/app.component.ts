3408f2664652d486f328fd5673a296e4a4ce88e8
Gian López <lopezarizagianlucas@gmail.com>
2021-07-03 12:30:48 -0500

    master: i just moved the lottie to the home page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,6 @@
 import { Component } from '@angular/core';
-import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app',
   templateUrl: './app.component.html'})
-export class AppComponent {
-
-  lottieOptions: AnimationOptions = {
-    path: '../assets/lotties/blue-waves.json'
-  };
-
-  lastScroll = 0;
-  hideLottie = false;
-
-  hideLottieOnScroll() {
-    let currentScroll = window.scrollY;
-    this.hideLottie = currentScroll > this.lastScroll;
-    console.log(this.hideLottie)
-    this.lastScroll = currentScroll;      
-  };
-
-};
+export class AppComponent {};
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,19 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
+import { AnimationOptions } from 'ngx-lottie';
 import { Observable } from 'rxjs';
 import { FetcherService } from 'src/app/services/fetcher/fetcher.service';
 import { Song } from 'src/app/shared/shared.models';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']})
 export class HomeComponent {
 
+  lottieOptions: AnimationOptions = {
+    path: '../assets/lotties/blue-waves.json'
+  };
+  
   song = '';
   songs!: Observable<Song[]>
 
