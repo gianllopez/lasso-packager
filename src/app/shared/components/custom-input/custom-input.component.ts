2f6808bd733397fe4dc949fc124a49fa52f84b9a
Gian López <lopezarizagianlucas@gmail.com>
2021-07-07 15:47:17 -0500

    master: the custom inputs have the info.

--- src/app/shared/components/custom-input/custom-input.component.ts
+++ src/app/shared/components/custom-input/custom-input.component.ts
@@ -7,6 +7,12 @@ import { Component, Input } from '@angular/core';
 export class CustomInputComponent {
 
   @Input() placeholder!: string;
+  @Input() name!: string;
   @Input() info!: string;
 
+  // Let the indent like this, it's not a dev error, it's a JS error.
+  message = `The songs audio are brought from Youtube
+and the tags from the most used music stream platforms,
+to create your song,you must to provide a Youtube MP3 URL.`;
+  
 };
