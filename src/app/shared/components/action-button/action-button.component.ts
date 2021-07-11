c611e8d220bfbc13611353e481fa8099e5249c8c
Gian López <lopezarizagianlucas@gmail.com>
2021-07-11 12:19:00 -0500

    master: i'll make some things in the back.

--- src/app/shared/components/action-button/action-button.component.ts
+++ src/app/shared/components/action-button/action-button.component.ts
@@ -7,5 +7,7 @@ import { Component, Input } from '@angular/core';
 export class ActionButtonComponent {
 
   @Input() goback!: boolean;
+  @Input() label!: string;
+  @Input() includeIcon = true;
   
 };
