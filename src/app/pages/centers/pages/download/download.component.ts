e80cf0740e255b283b2367035a6ee922e03158c8
Gian López <lopezarizagianlucas@gmail.com>
2021-09-08 01:26:55 -0500

    master: some reponsive things were added

--- src/app/pages/centers/pages/download/download.component.ts
+++ src/app/pages/centers/pages/download/download.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-download',
   templateUrl: './download.component.html',
   styleUrls: ['./download.component.scss'] })
-export class DownloadComponent {};
+export class DownloadComponent {
+
+  constructor(private deviceDetector: DeviceDetectorService) {};
+
+  get isMobile(): boolean {
+    return this.deviceDetector.isMobile();
+  };
+
+};
